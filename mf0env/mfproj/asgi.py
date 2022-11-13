319eb715a1e439cf17eb1f4ae40d2e257c6cbe62
Nickel <nj_eley@hotmail.com>
2022-11-13 17:34:38 +0000

    version0.1. for upload to github.  Changes include decouple and .env file.

--- mf0env/mfproj/asgi.py
+++ mf0env/mfproj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cbproj project.
+ASGI config for mfproj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfproj.settings')
 
 application = get_asgi_application()
--- mf0env/mfproj/wsgi.py
+++ mf0env/mfproj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cbproj project.
+WSGI config for mfproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfproj.settings')
 
 application = get_wsgi_application()
