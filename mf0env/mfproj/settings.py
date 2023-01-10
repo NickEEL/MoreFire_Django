87fc7df3f225c7252331d37dde13806bfcd0ff13
Nickel <nj_eley@hotmail.com>
2023-01-10 15:02:33 +0000

    1. Update settings.py for postgres db. 2. CSS change <p>

--- mf0env/mfproj/settings.py
+++ mf0env/mfproj/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'mfproj.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mf0db',
+        'USER': 'postgres',
+        'PASSWORD': 'pg_c0bblers',
+        'HOST': 'localhost',
+        'PORT': 5433,
     }
 }
 
